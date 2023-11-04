0a806c0ce27b14a8e02b324ffd2d2502a2dfbc66
salvaH21 <sholivares90@gmail.com>
2023-11-04 17:55:19 +0100

    seguimos igual

--- InterfazFFMPEG/src/interfazffmpeg/InterfazFFMPEG.java
+++ InterfazFFMPEG/src/interfazffmpeg/InterfazFFMPEG.java
@@ -3,9 +3,9 @@ package interfazffmpeg;
 public class InterfazFFMPEG {
 
     public static void main(String[] args) {
-        String comando = "C://Users//salva//Documents//FFMPEG//ffmpeg ejecutable//bin//ffmpeg.exe -i C://Users//salva//Documents//FFMPEG//Codificar//original.avi C://Users//salva//Documents//FFMPEG//Codificar//destino.mp4";
+        String comando = "C://Users//salva//Documents//FFMPEG//ffmpegejecutable//bin//ffmpeg.exe -i C://Codificar//original.avi C://Codificar//destino.mp4";
         try {
-            ProcessBuilder proceso = new ProcessBuilder("cmd.exe", "/c", comando);
+            ProcessBuilder proceso = new ProcessBuilder("cmd.exe","/c",comando);
             Process miproceso = proceso.start();
             System.out.println("ok nada");
         } catch (Exception e) {
